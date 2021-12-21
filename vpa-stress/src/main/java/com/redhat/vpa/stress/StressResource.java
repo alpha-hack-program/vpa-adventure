831bc3b5da17a0944cf84d2b132e8297e5c9b761
cvicens <carlos.vicens.alonso@gmail.com>
2021-12-21 10:41:23 +0100

    draft workers

--- vpa-stress/src/main/java/com/redhat/vpa/stress/StressResource.java
+++ vpa-stress/src/main/java/com/redhat/vpa/stress/StressResource.java
@@ -1,16 +1,39 @@
 package com.redhat.vpa.stress;
 
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+
+import javax.ws.rs.QueryParam;
+
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+
 @Path("/stress")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class StressResource {
 
+    @Inject
+    Work work;
+
+    
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        return "Hello RESTEasy";
+    }
+
+    @GET
+    @Path("memory")
+    public String memory(@QueryParam("start") Integer start, @QueryParam("end") Integer end, @QueryParam("period") Integer period) {
+
+        for(int i = 0; i < 10; i++) {
+            work.doWork();
+        }
+
         return "Hello RESTEasy";
     }
 }
